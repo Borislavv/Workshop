9625d0c0a209afad208d2b3cfae67bd730da697c
Boris <glazunov2142@icloud.com>
2021-03-28 18:42:22 +0500

    feat: BW-1 Add a description into main.go and a log message when starting the server

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -9,12 +9,17 @@ import (
 	"gitlab.com/Zendden/workshop/internal/handler"
 )
 
+/* App entrypoint */
 func main() {
+	/* Handler */
 	h := handler.NewHandler()
 
+	/* Handle routes */
 	r := chi.NewRouter()
 	r.Get("/", h.Index)
 
+	/* Start server */
+	log.Print("Server started")
 	err := http.ListenAndServe(":8080", r);
 	log.Fatal(err)
 }
