1d164dca8c08395d771d536e07967054bc4e4398
Boris <glazunov2142@icloud.com>
2021-04-17 13:44:46 +0500

    Add work wth Heroku env-variables

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"github.com/ilyakaznacheev/cleanenv"
 	"gitlab.com/Zendden/workshop/internal/api/joke"
 	"gitlab.com/Zendden/workshop/internal/config"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi"
 
@@ -23,7 +28,7 @@ func main() {
 	apiClient := joke.NewClient(cfg.JokeURL)
 
 	/* Handler */
-	h := handler.NewHandler(apiClient)
+	h := handler.NewHandler(apiClient, cfg.CustomeJoke)
 
 	/* Router */
 	r := chi.NewRouter()
@@ -33,8 +38,26 @@ func main() {
 
 	path := cfg.Host+":"+cfg.Port
 
+	server := &http.Server{
+		Addr: path,
+		Handler: r,
+	}
+
+	quit := make(chan os.Signal, 1)
+	done := make(chan error, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<- quit
+		ctx, _ := context.WithTimeout(context.Background(), time.Second * 15)
+		done <- server.Shutdown(ctx)
+	}()
+
 	/* Start server */
 	log.Printf("Server started as %s", path)
-	err = http.ListenAndServe(path, r)
-	log.Fatal(err)
+	_ = server.ListenAndServe()
+
+	err = <- done
+
+	log.Printf("shutting server down with: %v", err)
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -8,15 +8,22 @@ import (
 
 type Handler struct {
 	jokeClient api.Client
+	customeJoke string
 }
 
-func NewHandler(jokeClient api.Client) *Handler {
+func NewHandler(jokeClient api.Client, customeJoke string) *Handler {
 	return &Handler{
 		jokeClient: jokeClient,
+		customeJoke: customeJoke,
 	}
 }
 
 func (h *Handler) Index(w http.ResponseWriter, r *http.Request) {
+	if h.customeJoke != "" {
+		fmt.Fprint(w, h.customeJoke)
+		return
+	}
+
 	joke, err := h.jokeClient.Get()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
